e565abf8b76993d90e97ef17ac072d739d48ec1b
Hasan Hasanzade <hasanzade.web@gmail.com>
2023-11-27 16:05:27 +0400

    multer changed

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import routes from './routes/routes.js';
 import cors from 'cors';
-import 'dotenv/config'
+import 'dotenv/config';
+
 
 const app = express();
 
